0c3e0e4ca2ce8dbd16c66e4b9e4cebc5a6e6d57c
AndreyX1980 <aharybin@mail.ru>
2022-08-26 21:18:13 +0500

    отправка на проверку

---
+++ SEMINAR6/zadanie1/Program.cs
@@ -0,0 +1,6 @@
+﻿// сколько чисел больше "0" введено с клавиатуры
+Console.Write("Введите числа(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во чисел больше 0: {count}");
+
---
+++ SEMINAR6/zadanie3/Program.cs
@@ -0,0 +1,24 @@
+﻿// создать двумерный случайный массив (-10;10). Найти сумму чисел в главной диагонали.
+Console.WriteLine("Создаем случайный массив!");
+int[] a = new int[4];
+Random  rand1 = new Random();
+for(int i = 0; i < a.Length; i++)
+    a[i] = rand1.Next(-10, 10);
+int[] b = new int[4];
+Random  rand2 = new Random();
+for(int i = 0; i < b.Length; i++)
+    b[i] = rand2.Next(-10, 10);
+int[] c = new int[4];
+Random  rand3 = new Random();
+for(int i = 0; i < c.Length; i++)
+    c[i] = rand3.Next(-10, 10);
+string s = string.Join("  ", a);
+    Console.Write($"{s} ");
+
+string s1 = string.Join("  ", b);
+    Console.Write($"\n{s1} ");
+
+string s2 = string.Join("  ", c);
+    Console.Write($"\n{s2} ");
+
+Console.WriteLine($"\nСумма главной диагонали массива: {a[0]} + {b[1]} + {c[2]} = {a[0]+b[1]+c[2]}");
